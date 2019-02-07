0bbd6b4cf2da61d9ea74fe2db0ef77450ebd0aa1
Jakob Vyth <vyth@kth.se>
2019-02-07 16:47:21 +0100

    Add gitignore and template for problem solving

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+/**
+ * Code template for popup problem solving sessions.
+ * @author Carl Nyströmer (carlnys@kth.se)
+ * @author Jakob Vyth (vyth@kth.se)
+ */
+class Main {
+  public static void main(String[] args) {
+    Kattio kattio = new Kattio(System.in, System.out);
+
+    kattio.close();
+  }
+}
